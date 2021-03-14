1045ff995c90b59192c3809c161f720d6c658b96
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-14 14:52:01 +0100

    installed axios, created some components

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyA4ht3dZxUq7eEdlPnSNhJ6vXopPERrkzs';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  },
+});
